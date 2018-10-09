805b0e66af4b74585efbcac8459500938b2b203b
Rexman17 <a.stone1794@gmail.com>
2018-10-09 07:10:34 -0400

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,20 @@
 require 'pry'
 
 def my_all?(collection)
+  i = 0 # explicitly add a counter
+  block_return_values = [] # storing new calues
+  while i < collection.length
+    block_return_values <<
+yield(collection[i])
+    i += 1
+  end
 
+# add an #include? method to determine the return value of the my_all? method
+  if block_return_values.include?(false)
+      false
+    else
+      true
+    end
+# collection
+# => [1, 2, 3]
 end
